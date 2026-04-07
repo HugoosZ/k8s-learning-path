42ea4227bace14f50663729f2b8ab76d555e72bd
HugoosZ <132111633+HugoosZ@users.noreply.github.com>
2026-04-06 21:00:22 -0400

    feat: cambiar nginx por mi imagen de backend real

---
+++ src/backend/main.py
@@ -0,0 +1,25 @@
+import os
+import psycopg2
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/') 
+def test_db():
+    db_host = os.getenv('DB_HOST')
+    db_pass = os.getenv('DB_PASSWORD')
+    
+    try:
+        conn = psycopg2.connect(
+            host=db_host, 
+            password=db_pass, 
+            user="postgres", 
+            dbname="postgres",
+            connect_timeout=5 # Para que no se quede pegado si falla
+        )
+        return "<h1>¡Conexión exitosa a la DB desde el Backend!</h1>"
+    except Exception as e:
+        return f"<h1>Error de conexión: {e}</h1>", 500
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
